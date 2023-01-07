6ad6b6fea779b9bb0a8843a6bf7097e4d430d1ad
zaxe17 <119933185+zaxe17@users.noreply.github.com>
2023-01-07 21:25:12 +0800

    Add files via upload

---
+++ sheesh.js
@@ -0,0 +1,9 @@
+const audio = new Audio();
+audio.src = "scream.mp3";
+
+var typed = new Typed(".auto-type", {
+    strings: ["JAN MARC", "SOBERANO", "JACOLBIA"],
+    typeSpeed: 150,
+    backSpeed: 50,
+    loop: true
+})
